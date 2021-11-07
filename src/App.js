177f91231ac1603ee46fffcb8d56326526dfd21c
Shin Negishi <shinnegishi1986@gmail.com>
2021-11-07 10:06:58 +0000

    Commit new

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Notes from './pages/Notes'
 import Create from './pages/Create'
+import ReactDOM from 'react-dom';
+import Button from '@mui/material/Button';
+
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/">
+	  <Button variant="contained">Hello World</Button>;
           <Notes />
         </Route>
         <Route path="/create">
